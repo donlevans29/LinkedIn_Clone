05587fdd7de19edc4bb1665f8dd1e1b556ca5dd4
Javascript Don <donlevans29@gmail.com>
2022-04-04 16:34:18 -0500

    changes to file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; // importing Router
+import "./App.css"; // importing css
+import Login from "./components/Login"; // importing Login
 
 function App() {
   return (
     <div className="App">
-    <h2>Hello World</h2>
+      <Router>
+        <Switch>
+            <Route exact path="/" >
+              <Login />
+            </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Login =(props) => {
+  return <h2>Login</h2> 
+};
+
+export default Login;
